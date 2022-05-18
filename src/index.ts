4b58d575064e6144c6f05de1a797c8bd421b835f
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-05-18 14:31:45 -0300

    Calculadora

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,15 @@ let btnCalcular = document.getElementById("btnCalcular");
 let numero1 = document.getElementById("dato1.value");
 let numero2 = document.getElementById("dato2.value");
 let menuOpcion = document.getElementById("dato3.value");
+let resultado: number;
 
 function dibujarGuiones() {
   let i: number;
   let linea: string = "";
   for (i = 0; i < 40; i++) {
     linea = linea + "-";
-      };
-      console.log(linea);
+  }
+  console.log(linea);
 }
 
 btnCalcular.addEventListener("click", () => {
@@ -20,12 +21,12 @@ btnCalcular.addEventListener("click", () => {
   numero2 = Number(dato2.value);
   menuOpcion = Number(dato3.value);
   if (menuOpcion === 1) {
-    dibujarGuiones();
-    console.log("El resultado de la operacion es: " + (numero1+numero2));
-    dibujarGuiones();
+    resultado = numero1 + numero2;
   } else if (menuOpcion === 2) {
-    dibujarGuiones();
-    console.log("El resultado de la operacion es: " + (numero1-numero2));
-    dibujarGuiones();
-  } else 
+    resultado = numero1 - numero2;
+  } else {
+  }
+  dibujarGuiones();
+  console.log("El resultado de la operación es: " + resultado);
+  dibujarGuiones();
 });
